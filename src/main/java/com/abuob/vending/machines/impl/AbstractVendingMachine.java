94b5f945c1fe39a6582c8da33e7f8e09182f2ed6
Alex Buob <alexander.buob@gmail.com>
2019-12-15 17:44:48 -0600

    modifier change to only allow package access

--- src/main/java/com/abuob/vending/machines/impl/AbstractVendingMachine.java
+++ src/main/java/com/abuob/vending/machines/impl/AbstractVendingMachine.java
@@ -8,7 +8,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class AbstractVendingMachine {
+abstract class AbstractVendingMachine {
 
     protected VendingMachineHardwareFunctions vendingMachineHardwareFunctions;
     private Integer currentBalance = 0;
