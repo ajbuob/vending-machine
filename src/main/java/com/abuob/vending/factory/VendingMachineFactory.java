b232ad85efbcfff9c647af81525f9a40a52fea54
Alex Buob <alexander.buob@gmail.com>
2019-12-15 15:26:21 -0600

    initial add

--- src/main/java/com/abuob/vending/factory/VendingMachineFactory.java
+++ src/main/java/com/abuob/vending/factory/VendingMachineFactory.java
@@ -4,12 +4,15 @@ import com.abuob.vending.VendingMachine;
 import com.abuob.vending.VendingMachineHardwareFunctions;
 import com.abuob.vending.impl.VendingMachineImpl;
 
+import java.util.Objects;
+
 public final class VendingMachineFactory {
 
     private VendingMachineFactory() {
     }
 
     public static VendingMachine createVendingMachine(VendingMachineHardwareFunctions vendingMachineHardwareFunctions) {
-        return new VendingMachineImpl(vendingMachineHardwareFunctions);
+        return !Objects.isNull(vendingMachineHardwareFunctions) ?
+                new VendingMachineImpl(vendingMachineHardwareFunctions) : null;
     }
 }
---
+++ src/test/java/com/abuob/vending/factory/VendingMachineFactoryTest.java
@@ -0,0 +1,18 @@
+package com.abuob.vending.factory;
+
+import com.abuob.vending.impl.DefaultVendingMachineHardwareFunctions;
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class VendingMachineFactoryTest {
+
+    @Test
+    public void createVendingMachine_nullInput() {
+        assertThat(VendingMachineFactory.createVendingMachine(null)).isNull();
+    }
+
+    @Test
+    public void createVendingMachine_success() {
+        assertThat(VendingMachineFactory.createVendingMachine(new DefaultVendingMachineHardwareFunctions())).isNotNull();
+    }
+}
